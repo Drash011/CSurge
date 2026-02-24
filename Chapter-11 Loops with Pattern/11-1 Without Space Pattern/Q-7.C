7d549f2eb030f41c46cc7a9cb104908bc642dfa5
Drashti Thummar <thummard097@gmail.com>
2026-02-24 18:54:15 +0530

    Chapter-11

---
+++ Chapter-11 Loops with Pattern/11-1 Without Space Pattern/Q-7.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+main ()
+{
+	int i, j;
+
+	clrscr ();
+
+	for (i = 10; i <= 14; i++)
+	{
+		for (j = i; j <= 14; j++)
+		{
+			printf("%d\t", j);
+		}
+		printf("\n");
+	}
+
+	getch();
+}
