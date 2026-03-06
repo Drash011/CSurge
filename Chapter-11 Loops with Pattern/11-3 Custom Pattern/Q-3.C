4142137e807a98183726870867f635b1e063081d
Drashti Thummar <thummard097@gmail.com>
2026-03-06 11:40:45 +0530

    Chapter -11 Patterns

---
+++ Chapter-11 Loops with Pattern/11-3 Custom Pattern/Q-3.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+main ()
+{
+	int i, j, s;
+
+	clrscr();
+	printf("\n\n");
+	for (i = 5; i >= 1; i--)
+	{
+		for (j = 5; j >= i; j--)
+		{
+			printf("%d ", j);
+		}
+		printf("\n");
+	}
+
+	for (i = 2; i <= 5; i++)
+	{
+		for (j = 5; j >= i; j--)
+		{
+			printf("%d ", j);
+		}
+		printf("\n");
+	}
+
+	getch();
+}
