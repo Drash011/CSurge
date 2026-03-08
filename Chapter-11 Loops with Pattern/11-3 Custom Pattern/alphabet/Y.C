dcd46481ccb6ccc437e302540ba1e84e1ee18ea1
Drashti Thummar <thummard097@gmail.com>
2026-03-08 17:58:18 +0530

    Chapter - 11 Patterns

---
+++ Chapter-11 Loops with Pattern/11-3 Custom Pattern/alphabet/Y.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+main ()
+{
+	int i, j;
+	clrscr ();
+	printf("\n\n");
+
+	for (i = 1; i <= 7; i++)
+	{
+		for (j = 1; j <= 7; j++)
+		{
+			if ((i == 1 && (j == 1 || j == 7)) ||
+			    (i == 2 && (j == 2 || j == 6)) ||
+			    (i == 3 && (j == 3 || j == 5)) ||
+			    ((i >= 4 && i <= 7) && (j == 4)))
+				printf("*");
+			else
+				printf(" ");
+		}
+		printf("\n");
+	}
+
+	getch ();
+}
