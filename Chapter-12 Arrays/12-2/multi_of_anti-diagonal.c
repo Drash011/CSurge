d65069d3c43809911d46604129fae45f6bcffe7e
Drashti Thummar <thummard097@gmail.com>
2026-03-19 16:18:03 +0530

    Chapter - 12 Arrays

---
+++ Chapter-12 Arrays/12-2/multi_of_anti-diagonal.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+main ()
+{
+    int row, col;
+
+    printf("Enter row size :");
+    scanf("%d", &row);
+    
+    printf("Enter column size :");
+    scanf("%d", &col);
+
+    int a [row] [col];
+    int multi = 1;
+
+    printf("\n\nEnter array's elements :\n\n");
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("Enter a [%d] [%d] :", i, j);
+            scanf("%d", &a[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (i + j == row - 1)
+                printf("%d", a[i][j]);
+            
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (i + j == row - 1)
+            {
+                multi *= a[i][j];
+            }
+        }
+    }
+    
+    printf("\n\n");
+    printf("Multipication of anti-diagonal elements of an Array : %d", multi);
+    
+}
